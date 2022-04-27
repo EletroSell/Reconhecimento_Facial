45167133dc71770d46883b24547a06bd9ca4963a
Jackson cavalcante da silva <jackduos12@gmail.com>
2022-04-27 15:58:14 -0300

    começo agr sim
    começo agr sim

--- Site/Site/urls.py
+++ Site/Site/urls.py
@@ -1,4 +1,4 @@
-"""ReconhecimentoFacial URL Configuration
+"""Site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
